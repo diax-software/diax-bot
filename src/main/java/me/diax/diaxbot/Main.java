9b3ebc7954ed86685408173121bbe633bd04fef5
Comportment <Comportment@no-reply.github.com>
2017-04-17 00:41:12 +0100

    Let's have a little branch.

---
+++ src/main/java/me/diax/diaxbot/Main.java
@@ -0,0 +1,14 @@
+package me.diax.diaxbot;
+
+/**
+ * Created by Comportment on 16/04/17.
+ *
+ * Now with 80% more bugs!
+ */
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        new DiaxIRCBot(6667, "irc.domirc.net", "##testing", "#diax.me");
+    }
+}
