9fe5afcc438fe06c186c71fb8bbf83e7b05bf680
Comportment <Comportment@no-reply.github.com>
2017-04-16 23:05:12 +0100

    One classer.

--- src/main/java/me/diax/diaxbot/Main.java
+++ src/main/java/me/diax/diaxbot/Main.java
@@ -1,17 +1,62 @@
 package me.diax.diaxbot;
 
-import me.diax.diaxbot.bots.DiaxIRCBot;
+import java.io.*;
+import java.net.*;
 
-/**
- * Created by Comportment on 16/04/17.
- */
 public class Main {
 
-    public static void main(String[] args) {
-        new Main().main();
+    public static void main(String[] args) throws Exception {
+
+        // The server to connect to and our details.
+        String server = "irc.domirc.net";
+        String nick = "simple_bot";
+        String login = "simple_bot";
+
+        // The channel which the bot will join.
+        String channel = "#diax.me";
+
+        // Connect directly to the IRC server.
+        Socket socket = new Socket(server, 6667);
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(socket.getOutputStream( )));
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(socket.getInputStream( )));
+
+        // Log on to the server.
+        writer.write("NICK " + nick + "\r\n");
+        writer.write("USER " + login + " 8 * : Java IRC Hacks Bot\r\n");
+        writer.flush( );
+
+        // Read lines from the server until it tells us we have connected.
+        String line = null;
+        while ((line = reader.readLine( )) != null) {
+            if (line.indexOf("004") >= 0) {
+                // We are now logged in.
+                break;
+            }
+            else if (line.indexOf("433") >= 0) {
+                System.out.println("Nickname is already in use.");
+                return;
+            }
+        }
+
+        // Join the channel.
+        writer.write("JOIN " + channel + "\r\n");
+        writer.flush( );
+
+        // Keep reading lines from the server.
+        while ((line = reader.readLine( )) != null) {
+            if (line.toLowerCase( ).startsWith("PING ")) {
+                // We must respond to PINGs to avoid being disconnected.
+                writer.write("PONG " + line.substring(5) + "\r\n");
+                writer.write("PRIVMSG " + channel + " :I got pinged!\r\n");
+                writer.flush( );
+            }
+            else {
+                // Print the raw line received by the bot.
+                System.out.println(line);
+            }
+        }
     }
 
-    public void main() {
-        DiaxIRCBot bot = new DiaxIRCBot("irc.domirc.net", "#diax.me", 6697);
-    }
 }
--- src/main/java/me/diax/diaxbot/lib/bots/DiaxBot.java
+++
@@ -1,7 +0,0 @@
-package me.diax.diaxbot.lib.bots;
-
-/**
- * Created by comportment on 16/04/17.
- */
-public abstract class DiaxBot implements DiaxBotImpl {
-}
