e127547fe194c69f76cff3186ad757387855ed74
Comportment <Comportment@no-reply.github.com>
2017-04-17 15:10:14 +0100

    Gr8 bot here m8

--- src/main/java/me/diax/diaxbot/util/AbstractDiaxBot.java
+++ src/main/java/me/diax/diaxbot/util/AbstractDiaxBot.java
@@ -1,7 +1,9 @@
 package me.diax.diaxbot.util;
 
 /**
- * Created by comportment on 17/04/17.
+ * Created by Comportment on 17/04/17.
+ *
+ * Nomsy can do this better than me.
  */
 public class AbstractDiaxBot {
 }
