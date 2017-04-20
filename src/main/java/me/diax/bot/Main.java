229964bb06523a7bd087cb250049c39c5435085c
Comportment <Comportment@no-reply.github.com>
2017-04-20 22:34:06 +0100

    Sharding.

--- src/main/java/me/diax/bot/Main.java
+++ src/main/java/me/diax/bot/Main.java
@@ -5,8 +5,10 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.name.Names;
+import me.diax.bot.bots.DiaxDiscordBot;
 import me.diax.bot.bots.DiaxIRCBot;
 import me.diax.bot.lib.ComponentProvider;
+import me.diax.bot.lib.bot.AbstractDiaxAudioBot;
 import me.diax.bot.lib.bot.AbstractDiaxBot;
 import me.diax.bot.lib.command.DiaxCommandProvider;
 
@@ -31,14 +33,14 @@ public class Main implements ComponentProvider, Module {
 
     private void main() throws Exception {
         AbstractDiaxBot bot = injector.getInstance(DiaxIRCBot.class).start();
-        //AbstractDiaxAudioBot bot2 = new DiaxDiscordBot().start();
+        AbstractDiaxAudioBot bot2 = injector.getInstance(DiaxDiscordBot.class).start();
     }
 
     @Override
     public void configure(Binder binder) {
         binder.bind(ComponentProvider.class).toInstance(this);
-        binder.bind(String.class).annotatedWith(Names.named("prefix")).toInstance("<>");
         binder.bind(DiaxCommandProvider.class).to(DiaxCommandHandler.class);
+        binder.bind(String.class).annotatedWith(Names.named("prefix")).toInstance("<>");
     }
 
     @Override
--- src/main/java/me/diax/bot/bots/DiaxDiscordBot.java
+++ src/main/java/me/diax/bot/bots/DiaxDiscordBot.java
@@ -1,5 +1,6 @@
 package me.diax.bot.bots;
 
+import com.mashape.unirest.http.Unirest;
 import me.diax.bot.Main;
 import me.diax.bot.lib.bot.AbstractDiaxAudioBot;
 import me.diax.bot.lib.command.DiaxCommandProvider;
@@ -11,12 +12,19 @@ import me.diax.bot.lib.objects.DiaxMessage;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.OnlineStatus;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
 import javax.inject.Inject;
+import javax.inject.Named;
 import javax.inject.Singleton;
+import javax.security.auth.login.LoginException;
 import java.sql.Timestamp;
+import java.util.Arrays;
 
 /**
  * Created by comportment on 17/04/17.
@@ -26,33 +34,53 @@ import java.sql.Timestamp;
 @Singleton
 public class DiaxDiscordBot extends AbstractDiaxAudioBot {
 
-    private static JDA jda;
+    private static JDA[] SHARDS;
+    private static String TOKEN = "---";
+    private final String prefix;
     private DiaxCommandProvider handler;
 
     @Inject
-    public DiaxDiscordBot(DiaxCommandProvider handler) {
+    public DiaxDiscordBot(DiaxCommandProvider handler, @Named("prefix") String prefix) {
         this.handler = handler;
+        this.prefix = prefix;
     }
 
     @Override
     public DiaxDiscordBot start() throws Exception {
         if (this.hasStarted()) throw new BotStartException("Bot has already started.");
         System.out.println("Starting...");
-        jda = new JDABuilder(AccountType.BOT).setToken("---").addEventListener(
-                new ListenerAdapter() {
-                    @Override
-                    public void onMessageReceived(MessageReceivedEvent event) {
-                        DiaxMessage message = new DiaxMessage(
-                                new DiaxAuthor(
-                                        event.getAuthor().getAvatarId(),
-                                        event.getAuthor().getName()),
-                                event.getMessage().getRawContent(),
-                                new Timestamp(1000 * event.getMessage().getCreationTime().toEpochSecond()),
-                                new DiaxChannel(event.getChannel().getId(), event.getChannel().getName()));
-                        handler.execute(new Main().getInstance(DiaxDiscordBot.class), message);
-                    }
-                }
-        ).buildBlocking();
+        int amount = getRecommendedShards();
+        SHARDS = new JDA[amount >= 3 ? amount : 1];
+        for (int i = 0; i < SHARDS.length; i++) {
+            JDA jda = null;
+            try {
+                JDABuilder builder = new JDABuilder(AccountType.BOT)
+                        .addEventListener(new ListenerAdapter() {
+                            @Override
+                            public void onMessageReceived(MessageReceivedEvent event) {
+                                DiaxMessage message = new DiaxMessage(
+                                        new DiaxAuthor(
+                                                event.getAuthor().getAvatarId(),
+                                                event.getAuthor().getName()),
+                                        event.getMessage().getRawContent(),
+                                        new Timestamp(1000 * event.getMessage().getCreationTime().toEpochSecond()),
+                                        new DiaxChannel(event.getChannel().getId(), event.getChannel().getName()));
+                                handler.execute(new Main().getInstance(DiaxDiscordBot.class), message);
+                            }
+                        })
+                        .setAudioEnabled(true)
+                        .setGame(Game.of(prefix + "help"))
+                        .setToken(TOKEN)
+                        .setStatus(OnlineStatus.ONLINE);
+                if (SHARDS.length >= 3)
+                    builder.useSharding(i, SHARDS.length);
+                jda = builder.buildBlocking();
+            } catch (LoginException | RateLimitedException | InterruptedException ignored) {
+            }
+            if (jda != null) {
+                SHARDS[i] = jda;
+            }
+        }
         this.setStarted(true);
         return this;
     }
@@ -61,14 +89,34 @@ public class DiaxDiscordBot extends AbstractDiaxAudioBot {
     public DiaxDiscordBot stop() throws Exception {
         if (!this.hasStarted()) throw new BotStopException("Bot has already stopped.");
         System.out.println("Stopping...");
-        jda.shutdown(false);
+        Arrays.stream(SHARDS).forEach(shard -> {
+            try {
+                shard.shutdown(false);
+            } catch (Exception ignored) {
+            }
+        });
         this.setStarted(false);
         return this;
     }
 
     @Override
     public DiaxDiscordBot messageTo(DiaxChannel channel, String message) throws Exception {
-        jda.getTextChannelById(channel.getIdentifier()).sendMessage(message).queue();
+        findChannel(Long.valueOf(channel.getIdentifier())).sendMessage(message).queue();
         return this;
+    }
+
+    private int getRecommendedShards() {
+        try {
+
+            return Unirest.get("https://discordapp.com/api/gateway/bot")
+                    .header("Authorization", "Bot " + TOKEN)
+                    .header("Content-Type", "application/json").asJson().getBody().getObject().getInt("shards");
+        } catch (Exception e) {
+            return -1;
+        }
+    }
+
+    private TextChannel findChannel(long id) {
+        return Arrays.stream(SHARDS).flatMap(shard -> shard.getTextChannels().stream()).filter(textChannel -> textChannel.getIdLong() == id).findFirst().get();
     }
 }
